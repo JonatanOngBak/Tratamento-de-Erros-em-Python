2f7613fbde955a9002f30dd223f538c0e540e0f1
Jonatan Fernandes <jonathanfds2007@gmail.com>
2024-07-29 08:15:21 -0300

    Adicionado arquivo de aula23.py

---
+++ Aula23.py
@@ -0,0 +1,12 @@
+#print(x)   # NameError: name 'x' is not defined
+
+# erros são EXCEÇÃO 
+
+#n = int(input('Número: '))
+#print(f'Você didigitou o número {n}') # oito = ValueError: invalid literal for int()
+
+a = int(input('Númerador: '))
+b = int(input('Denominador: '))
+r = a / b
+print(f'O resultado é {r}')
+
