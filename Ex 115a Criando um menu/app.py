9adf94c9e5b885416bb38fbc1710af1692f92271
Jonatan Fernandes <jonathanfds2007@gmail.com>
2024-07-31 12:11:14 -0300

    Adicionado lista de pessoas

--- Ex 115a Criando um menu/app.py
+++ Ex 115a Criando um menu/app.py
@@ -1,12 +1,19 @@
 """Vamos criar um menu em Python, usando modularização."""
 
 from lib.interface import *
+from lib.Arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)   
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        # opcão de listar o conteudo de um arquivo!
+        lerAquivo(arq)
     elif resposta == 2:
         cabeçalho('Opção 2')
     elif resposta == 3:
@@ -14,6 +21,4 @@ while True:
         break
     else:
         print('\033[31mERRO! Digite uma opção válida!\033[m') 
-    sleep(2)         
-
-
+    sleep(2)
